09f24b55ff88e59ca5709947593329605ab6a2d7
MrSaan <langlangbolang87@gmail.com>
2021-10-02 17:29:54 +0700

    fix dropdown 2

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,16 +1,10 @@
 @props(['active' => false])
 
 @php
-    $defaultClasses = 'block text-left px-3 text-sm leading-6 hover:bg-blue-500 fokus:bg-blue-500 hover:text-white fokus:text-white';
+    $classes = 'block text-left px-3 text-sm leading-6 hover:bg-blue-500 fokus:bg-blue-500 hover:text-white fokus:text-white';
 
-    $activeClasses = 'bg-blue-500 text-white';
-    
-    // cara lama
-    // if ($active) $defaultClasses .= ' bg-blue-500 text-white';
+    if ($active) $classes .= ' bg-blue-500 text-white';
 @endphp
 
-<a {{ $attributes->classes([$defaultClasses, $activeClasses => $active]) }}>{{ $slot }}</a>
-
-{{-- cara lama --}}
-{{-- <a {{ $attributes(['class' => $defaultClasses])}}
-    >{{ $slot }}</a> --}}
+<a {{ $attributes(['class' => $classes])}}
+    >{{ $slot }}</a>
